a029be44ef332e0bdf776165c5b52f66c6316153
abrar39 <free.lancer.engineer01@gmail.com>
2024-01-06 23:08:11 +0500

    Custom Module called Estate Added. Also, First model created.

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Estate Management", #this is the only required field
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Abrar Asghar",
+    'category': 'Real Estate',
+    'description': """
+    Custom Estate Management Module.
+    """,
+    'application' : True,
+    # data files always loaded at installation
+    #'data': [
+    #    'views/mymodule_view.xml',
+    #],
+    # data files containing optionally loaded demonstration data
+    #'demo': [
+    #    'demo/demo_data.xml',
+    #],
+}
